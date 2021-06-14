f0f11555a9eaa9649e1aaa089570a9fc9468a894
R.Moriyama <moriforest537@gmail.com>
2021-06-14 20:53:28 +0900

    wip

---
+++ myblogapp/posts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    # return HttpResponse("Hello world　このページは投稿のインデックスです。")
+    return render(request, 'posts/index.html') 
+
+# Create your views here.
