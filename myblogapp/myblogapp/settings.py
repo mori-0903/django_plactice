ca2310c5db6290df7ed6d48d1a456f665a90edf5
R.Moriyama <moriforest537@gmail.com>
2021-06-16 07:28:35 +0900

    wip

--- myblogapp/myblogapp/settings.py
+++ myblogapp/myblogapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myblogapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL  = '/pics/'
+MEDIA_ROOT = BASE_DIR 
